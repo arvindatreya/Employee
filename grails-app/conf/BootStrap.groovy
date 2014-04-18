d385e23fb029cabc8fbfa31f6c99f01c6c7c12b7
Chaitanya Atreya <atreya@gmail.com>
2014-04-17 20:18:44 -0700

    Adding a bootstrap employee row

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,15 @@
+import employee.ListEmployee
+
 class BootStrap {
 
     def init = { servletContext ->
+		new ListEmployee(
+			emp_id: 1,
+			emp_email: 'hello@world.com',
+			emp_name: 'Some name',
+			emp_phone: '123456789',
+			emp_address: 'Some address',
+			emp_salary: '100,000').save(failOnError: true)
     }
     def destroy = {
     }
