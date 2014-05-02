8e71d18db26aa4b1255aad309c736376c203df7c
unknown <ATR61590@US000761AA.hmmg.cc>
2014-05-02 09:35:36 -0400

    Second Commit

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,15 +2,19 @@ import employee.ListEmployee
 
 class BootStrap {
 
+	
     def init = { servletContext ->
 		new ListEmployee(
 			emp_id: 1,
-			emp_email: 'hello@world.com',
-			emp_name: 'Some name',
+			emp_email: 'Supreetha.sudhakar@symbiance.com',
+			emp_name: 'Supreetha Sudhakar',
 			emp_phone: '123456789',
-			emp_address: 'Some address',
+			emp_address: '520 Birchwood Court North Brunswick NJ 08902',
 			emp_salary: '100,000').save(failOnError: true)
     }
+	
+
+	
     def destroy = {
     }
 }
